2a08cd2d84ea1fb96e449983e34f1eac12cf5c6e
allisonbusse <allisonbusse@gmail.com>
2019-07-30 16:21:15 -0700

    populated products page

--- src/data/products.js
+++ src/data/products.js
@@ -1,70 +1,122 @@
 const bag = {
     id: 'bag',
     name: 'Star Wars Suitcase',
-    image: 'assets/bag.jpg'
+    image: './assets/bag.jpg'
 };
 
 const banana = {
     id: 'banana',
     name: 'Banana Slicer',
-    image: 'assets/banana.jpg', 
+    image: './assets/banana.jpg', 
 };
 
 const bathroom = {
     id: 'bathroom',
     name: 'Bathroom iPad Stand',
-    image: 'assets/bathroom.jpg'
+    image: './assets/bathroom.jpg'
 };
 
 const boots = {
     id: 'boots',
     name: 'Open-toed Rain Boots',
-    image: 'assets/boots.jpg'
+    image: './assets/boots.jpg'
 };
 
 const breakfast = {
     id: 'breakfast',
     name: 'All-in-one Breakfast Machine',
-    image: 'assets/breakfast.jpg'
+    image: './assets/breakfast.jpg'
 };
 
 const bubblegum = {
     id: 'bubblegum',
     name: 'Meatball Bubble Gum',
-    image: 'assets/bubblegum.jpg'
+    image: './assets/bubblegum.jpg'
 };
 
 const chair = {
     id: 'chair',
     name: 'Inverted Chair',
-    image: 'assets/chair.jpg'
+    image: './assets/chair.jpg'
 };
 
 const cthulhu = {
     id: 'cthulhu',
     name: 'Cthulhu Action Figure',
-    image: 'assets/cthulhu.jpg'
+    image: './assets/cthulhu.jpg'
 };
 
 const dogDuck = {
     id: 'dog-duck',
     name: 'Dog Duck Lips',
-    image: 'assets/dog-duck.jpg'
+    image: './assets/dog-duck.jpg'
 };
 
 const dragon = {
     id: 'dragon',
     name: 'Can of Dragon Meat',
-    image: 'assets/dragon.jpg'
+    image: './assets/dragon.jpg'
 };
 
 const pen = {
     id: 'pen',
     name: 'Utensil Pen',
-    image: 'assets/pen.jpg'
+    image: './assets/pen.jpg'
 };
 
+const petSweep = {
+    id: 'pet-sweep',
+    name: 'Pet Sweep Pads',
+    image: './assets/pet-sweep.jpg'
+};
 
+const scissors = {
+    id: 'scissors',
+    name: 'Pizza Scissors',
+    image: './assets/scissors.jpg'
+};
+
+const shark = {
+    id: 'shark',
+    name: 'Shark Sleeping Bag',
+    image: './assets/shark.jpg'
+};
+
+const sweep = {
+    id: 'sweep',
+    name: 'Baby Sweep Onesie',
+    image: './assets/sweep.png'
+};
+
+const tauntaun = {
+    id: 'tauntaun',
+    name: 'Tauntaun Sleeping Bag',
+    image: './assets/tauntaun.jpg'
+};
+
+const unicorn = {
+    id: 'unicorn',
+    name: 'Unicorn Meat',
+    image: './assets/unicorn.jpg'
+};
+
+const usb = {
+    id: 'usb',
+    name: 'USB Tail',
+    image: './assets/usb.gif'
+};
+
+const waterCan = {
+    id: 'water-can',
+    name: 'Inverse Water Can',
+    image: './assets/water-can.jpg'
+};
+
+const wineGlass = {
+    id: 'wine-glass',
+    name: 'The Ultimate Snifter',
+    image: './assets/wine-glass.jpg'
+};
 
 const products = [
     bag,
@@ -77,7 +129,16 @@ const products = [
     cthulhu,
     dogDuck,
     dragon,
-    pen
+    pen,
+    petSweep,
+    scissors,
+    shark,
+    sweep,
+    tauntaun,
+    unicorn,
+    usb,
+    waterCan,
+    wineGlass
 ];
 
 export default products;
